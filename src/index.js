1a48c44161e37ec099b15737895e5e7af937f827
Mikhail Lobotskiy <Mikhail.Lobotskiy@onlyoffice.com>
2024-03-06 00:12:31 +0400

    multiple views handling

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ app.post('/', (req, res) => {
     data.id = msgId++;
     activeMsgs.set(data.id, data);
     msgSet.add(data.message);
-    sendDataToView(0, data);    // send message to view
+    sendDataToAllViews(0, data);    // send message to all views
     res.status(200).send();
 });
 
@@ -56,52 +56,54 @@ app.listen(3000, () => {
 });
 
 // WebSocket server
-const wss = new WebSocketServer({ port: 3001 });
-var ws = null;
+const wss = new WebSocketServer({ port: 3001, clientTracking: true });
 
-wss.on('connection', (websocket) => {
-    ws = websocket;
+wss.on('connection', (ws) => {
     console.log('[WebSocket] View connected');
 
-    // send every active message to view
+    // send every active message to newly connected view
     for (entry of activeMsgs) {
-        sendDataToView(0, entry[1]);
+        sendDataToView(ws, 0, entry[1]);
     }
 
     ws.on('message', (rawData) => {
         console.log('[WebSocket] Recieved: \n%s', rawData);
         const data = JSON.parse(rawData);
         switch (data.type) {
-            case 1:             // view requires authors
-                sendDataToView(1, Array.from(authorsSet));  // send authors array
+            case 1:                                                 // view requires authors
+                sendDataToView(ws, 1, Array.from(authorsSet));      // send authors array
                 break;
-            case 2:             // view discards message with saving author
-                // data.data = id
+            case 2:                                                 // view discards message with saving author
                 authorsSet.add(activeMsgs.get(data.data).author);
-            case 3:             // view discards message without saving
-                // data.data = id
+            case 3:                                                 // view discards message without saving
                 activeMsgs.delete(data.data);
+                // send instruction to remove this message to all views (except the one from which this event came)
+                for (ws_ of wss.clients) {
+                    if (ws !== ws_) {
+                        sendDataToView(ws_, 2, { msgId: data.data, accept: data.type === 2 });
+                    }
+                }
                 break;
             default:
                 break;
         }
     });
 
-    // TODO: seems that it causes a bug with separate views.
     ws.on('close', (reason) => {
-        ws = null;
         console.log('[WebSocket] View closed. Reason: %s', reason);
     });
 });
 
-function sendDataToView(type, data) {
-    if (ws) {
-        ws.send(JSON.stringify({
-            type: type,
-            data: data
-        }));
-    }
+function sendDataToView(ws, type, data) {
+    ws.send(JSON.stringify({
+        type: type,
+        data: data
+    }));
 }
 
-// TODO:
-// - fix bug above
+function sendDataToAllViews(type, data) {
+    console.log('Sending to all %s WS connections', wss.clients.size);
+    for (ws of wss.clients) {
+        sendDataToView(ws, type, data);
+    }
+}
